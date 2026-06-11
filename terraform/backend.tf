1cc9d51337360b0f73400d863dd6324b36fd0f45
Mazen2004212 <mazenibrahim213@gmail.com>
2026-06-12 00:01:01 +0300

    Add Terraform S3 remote backend with native locking

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+﻿terraform {
+  backend "s3" {
+    bucket       = "cloudcart-tfstate-766696030212-us-east-1"
+    key          = "cloudcart/eks/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
